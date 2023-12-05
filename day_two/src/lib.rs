e65de77243d3ddd34aa77fdcee7a5c1a5b7b06ae
Matthew Griffin <matthewjamesgriffin@gmail.com>
2023-12-05 13:37:59 +0000

    Day 2 part 2

--- day_two/src/lib.rs
+++ day_two/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
 pub fn part_one(input: &str) -> u32 {
     let limits: HashMap<&str, u32> = HashMap::from([
@@ -24,21 +24,51 @@ pub fn part_one(input: &str) -> u32 {
     .sum()
 }
 
+pub fn part_two(input: &str) -> u32 {
+    input.lines()
+    .map(|line| {
+        let mut min_cubes: HashMap<&str, u32> = HashMap::new();
+
+        let parts: Vec<_> = line.split(":").collect();
+        let cube_counts = parts[1].split(&[';', ','][..]);
+        cube_counts.for_each(|cube_count| {
+            let mut cube_info = cube_count.trim().split_whitespace();
+            let cube_count = cube_info.next().unwrap().parse::<u32>().unwrap();
+            let cube_colour = cube_info.next().unwrap();
+            min_cubes.entry(cube_colour).and_modify(|existing| {
+              if cube_count > *existing {
+                *existing = cube_count
+              }  
+            }).or_insert(cube_count);
+        });
+        min_cubes.values().cloned().reduce(|acc, e| acc * e).unwrap()
+    })
+    .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     use indoc::indoc;
 
+    fn test_input() -> &'static str {
+        indoc! {"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+                Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+                Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+                Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"}
+    }
+
     #[test]
     fn it_sums_ids_of_possible_games() {
-        const INPUT: &str = indoc! {"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-                                    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-                                    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-                                    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-                                    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"};
-
-        let result = part_one(INPUT);
+        let result = part_one(test_input());
         assert_eq!(result, 8);
+    }
+
+    #[test]
+    fn it_sums_power_of_minimum_sets() {
+        let result = part_two(test_input());
+        assert_eq!(result, 2286);
     }
 }
--- main/src/main.rs
+++ main/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
 
     let input = fs::read_to_string("input2.txt").unwrap();
     let part1 = day_two::part_one(&input);
-    //let part2 = day_two::part_two(&input);
+    let part2 = day_two::part_two(&input);
     println!("Day 2, Part 1: {part1}");
-    //println!("Day 2, Part 2: {part2}");    
+    println!("Day 2, Part 2: {part2}");    
 }
