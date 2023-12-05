80713427fbf3a8a7a882aac2798458481aaee07b
Matthew Griffin <matthewjamesgriffin@gmail.com>
2023-12-05 14:34:18 +0000

    Allow main to run all days or one specified

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,15 +1,42 @@
-use std::fs;
+use std::{fs, env, collections::{BTreeMap, btree_map::Entry}};
 
-fn main() {
-    let input = fs::read_to_string("input1.txt").unwrap();
+fn day_one(input: &str) {
     let part1 = day_one::part_one(&input);
     let part2 = day_one::part_two(&input);
     println!("Day 1, Part 1: {part1}");
     println!("Day 1, Part 2: {part2}");
+}
 
-    let input = fs::read_to_string("input2.txt").unwrap();
+fn day_two(input: &str) {
     let part1 = day_two::part_one(&input);
     let part2 = day_two::part_two(&input);
     println!("Day 2, Part 1: {part1}");
-    println!("Day 2, Part 2: {part2}");    
+    println!("Day 2, Part 2: {part2}");  
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let day_number = match args.len() {
+        0 | 1 => 0,
+        _ => args[1].parse::<u8>().unwrap_or(0)
+    };
+
+    let mut days: BTreeMap<u8, fn(&str)> = BTreeMap::new();
+    days.insert(1, day_one);
+    days.insert(2, day_two);
+
+    match days.entry(day_number) {
+        Entry::Occupied(day) => {
+            let input_file = format!("input{day_number}.txt");
+            let input = fs::read_to_string(input_file).unwrap();
+            day.get()(&input);
+        },
+        Entry::Vacant(_) => {
+            for (day_number, func) in days.iter() {
+                let input_file = format!("input{day_number}.txt");
+                let input = fs::read_to_string(input_file).unwrap();
+                func(&input);
+            }
+        },
+    } 
 }
