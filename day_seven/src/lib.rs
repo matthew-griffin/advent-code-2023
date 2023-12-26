2c2cbdf8750652364a9757ee8a5f3b5fa71d6839
Matthew Griffin <matthewjamesgriffin@gmail.com>
2023-12-26 21:41:30 +0000

    Day 7 part 2

--- day_seven/src/lib.rs
+++ day_seven/src/lib.rs
@@ -1,25 +1,5 @@
 use std::{collections::HashMap, cmp::Ordering, iter::zip};
 
-use lazy_static::lazy_static;
-
-lazy_static! {
-    static ref CARD_MAP: HashMap<char, u64> = HashMap::from([
-        ('2', 1),
-        ('3', 2),
-        ('4', 3),
-        ('5', 4),
-        ('6', 5),
-        ('7', 6),
-        ('8', 7),
-        ('9', 8),
-        ('T', 9),
-        ('J', 10),
-        ('Q', 11),
-        ('K', 12),
-        ('A', 13),
-    ]);
-}
-
 pub struct Solution;
 
 impl common::Solution for Solution {
@@ -44,6 +24,18 @@ impl<'a> Hand<'a> {
         let cards = parts.next().unwrap();
         let bid = parts.next().unwrap().parse::<u64>().unwrap();
         let strength = Hand::calc_hand_strength(cards);
+        Hand {
+            cards,
+            bid,
+            strength
+        }
+    }
+
+    fn parse_part_two(input: &'a str) -> Hand<'a> {
+        let mut parts = input.split_whitespace();
+        let cards = parts.next().unwrap();
+        let bid = parts.next().unwrap().parse::<u64>().unwrap();
+        let strength = Hand::calc_part_two_hand_strength(cards);
         Hand {
             cards,
             bid,
@@ -78,10 +70,47 @@ impl<'a> Hand<'a> {
         }
     }
 
-    fn order_by_sub_strength(&self, other: &Self) -> Ordering {
+    fn calc_part_two_hand_strength(input: &str) -> u64 {
+        let mut card_map: HashMap<char, u8> = HashMap::new();
+        for card in input.chars() {
+            *card_map.entry(card).or_insert(0) += 1;
+        }
+        let joker_count = card_map.remove(&'J').unwrap_or(0);
+        let max_key = card_map
+            .iter()
+            .max_by(|a, b| a.1.cmp(&b.1))
+            .map(|(k, _v)| k);
+        match max_key {
+            Some(key) => {*card_map.entry(*key).or_default() += joker_count;},
+            None => return 6,
+        }
+        match card_map.len() {
+            1 => 6,
+            2 => {
+                if card_map.iter().any(|(_, count)| *count > 3) {
+                    5
+                } else {
+                    4
+                }
+            },
+            3 => {
+                if card_map.iter().any(|(_, count)| *count > 2) {
+                    3
+                } else {
+                    2
+                }
+            },
+            4 => 1,
+            5 => 0,
+            x => panic!("Should not have {x} cards!"),
+        }
+ 
+    }
+
+    fn order_by_sub_strength(&self, other: &Self, card_map: &HashMap<char, u8>) -> Ordering {
         for (l, r) in zip(self.cards.chars(), other.cards.chars()) {
-            let l_sub = CARD_MAP.get(&l).unwrap();
-            let r_sub = CARD_MAP.get(&r).unwrap();
+            let l_sub = card_map.get(&l).unwrap();
+            let r_sub = card_map.get(&r).unwrap();
             if l_sub > r_sub {
                 return Ordering::Greater;
             } else if r_sub > l_sub {
@@ -93,6 +122,9 @@ impl<'a> Hand<'a> {
 }
 
 fn part_one(input: &str) -> u64 {
+    let card_map = create_card_map(&[
+        '2','3','4','5','6','7','8','9','T','J','Q','K','A'
+    ]);
     let mut hands: Vec<_> = input.lines()
     .map(Hand::parse_new)
     .collect();
@@ -102,7 +134,7 @@ fn part_one(input: &str) -> u64 {
         } else if l_hand.strength < r_hand.strength {
             Ordering::Less
         } else {
-            l_hand.order_by_sub_strength(r_hand)
+            l_hand.order_by_sub_strength(r_hand, &card_map)
         }
     });
     hands.iter().enumerate()
@@ -111,8 +143,34 @@ fn part_one(input: &str) -> u64 {
     })
 }
 
-fn part_two(_input: &str) -> u64 {
-    0
+fn part_two(input: &str) -> u64 {
+    let card_map = create_card_map(&[
+        'J','2','3','4','5','6','7','8','9','T','Q','K','A'
+    ]);
+    let mut hands: Vec<_> = input.lines()
+    .map(Hand::parse_part_two)
+    .collect();
+    hands.sort_by(|l_hand, r_hand| {
+        if l_hand.strength > r_hand.strength {
+            Ordering::Greater
+        } else if l_hand.strength < r_hand.strength {
+            Ordering::Less
+        } else {
+            l_hand.order_by_sub_strength(r_hand, &card_map)
+        }
+    });
+    hands.iter().enumerate()
+    .fold(0, |acc, (index, hand)| {
+        acc + (index as u64 + 1) * hand.bid
+    })
+}
+
+fn create_card_map(char_order: &[char]) -> HashMap<char, u8> {
+    let mut result = HashMap::new();
+    for (index, character) in char_order.iter().enumerate() {
+        result.insert(*character, index as u8);
+    }
+    result
 }
 
 #[cfg(test)]
@@ -132,5 +190,11 @@ mod tests {
     fn it_multiplies_bid_by_rank() {
         let result = part_one(test_input());
         assert_eq!(result, 6440);
+    }
+
+    #[test]
+    fn it_multiplies_bid_by_rank_using_jokers() {
+        let result = part_two(test_input());
+        assert_eq!(result, 5905);
     }
 }
